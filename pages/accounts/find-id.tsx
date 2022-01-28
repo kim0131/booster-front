a4b0d1aee65dcc2c77f8fd4f3ac7e3be0f02416b
shjeon-datahive <sh.jeon@datahive.co.kr>
2022-01-28 10:42:18 +0900

    불필요한 import 삭제

--- pages/accounts/find-id.tsx
+++ pages/accounts/find-id.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { jsx, css, Global, ClassNames } from "@emotion/react";
 import Header from "../../component/header";
 import Footer from "../../component/footer";
--- pages/my-booster/[category].tsx
+++ pages/my-booster/[category].tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { jsx, css, Global, ClassNames } from "@emotion/react";
 import { useRouter } from "next/router";
 import Header from "../../component/header";
