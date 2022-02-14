f3462fd4331d88b6ebbb0f249a46aac706d90cd4
limsoomin <lsmism10@icloud.com>
2022-02-14 12:54:20 +0900

    헤더, 푸터 정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Header from "@components/templates/header";
-import Footer from "@components/templates/footer";
 import Button from "@components/elements/button";
 import TextField from "@components/elements/text-field";
 import { IconAdd } from "@components/icons";
@@ -9,52 +7,47 @@ import useToast from "@core/hook/use-toast";
 const Home: NextPage = () => {
   const toast = useToast();
   return (
-    <>
-      <Header />
-
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "8px",
-          padding: "1rem",
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "8px",
+        padding: "1rem",
+      }}
+    >
+      <Button isDisabled>
+        <IconAdd />
+        {toast.message}
+      </Button>
+      <Button
+        variants="ghost"
+        color="primary"
+        size="small"
+        isLoading={true}
+        onClick={() => console.log("ddd")}
+      >
+        <IconAdd />
+        ㅠㅠ
+      </Button>
+      <Button
+        variants="solid"
+        color="primary"
+        size="large"
+        onClick={() => {
+          toast.mutate("성공");
         }}
       >
-        <Button isDisabled>
-          <IconAdd />
-          {toast.message}
-        </Button>
-        <Button
-          variants="ghost"
-          color="primary"
-          size="small"
-          isLoading={true}
-          onClick={() => console.log("ddd")}
-        >
-          <IconAdd />
-          ㅠㅠ
-        </Button>
-        <Button
-          variants="solid"
-          color="primary"
-          size="large"
-          onClick={() => {
-            toast.mutate("성공");
-          }}
-        >
-          <IconAdd />
-          ㅠㅠ
-        </Button>
-        <TextField
-          label="dada"
-          prefix={<IconAdd />}
-          size="large"
-          placeholder="test"
-          error="아아하하"
-        />
-      </div>
-      <Footer />
-    </>
+        <IconAdd />
+        ㅠㅠ
+      </Button>
+      <TextField
+        label="dada"
+        prefix={<IconAdd />}
+        size="large"
+        placeholder="test"
+        error="아아하하"
+      />
+    </div>
   );
 };
 
