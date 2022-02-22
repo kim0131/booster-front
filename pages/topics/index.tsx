7871384848e85eda0cd5e7f6bd71196336fc9736
shjeon-datahive <sh.jeon@datahive.co.kr>
2022-02-22 17:42:53 +0900

    토픽 Lnb 메뉴 출력

--- pages/topics/index.tsx
+++ pages/topics/index.tsx
@@ -1,36 +1,48 @@
 import LnbLayout from "@components/layouts/lnb-layout";
 import Board from "@components/templates/board";
 import Lnb from "@components/templates/lnb";
+import axios from "axios";
 import type { NextPage } from "next";
-
-const lnbDatas = [
-  {
-    id: 0,
-    category: "카테고리1",
-    menus: [
-      { id: 0, content: "메뉴1", param: "menu1" },
-      { id: 1, content: "메뉴2", param: "menu2" },
-      { id: 2, content: "메뉴3", param: "menu3" },
-      { id: 3, content: "메뉴4", param: "menu4" },
-    ],
-  },
-  {
-    id: 1,
-    category: "카테고리2",
-    menus: [
-      { id: 0, content: "메뉴5", param: "menu5" },
-      { id: 1, content: "메뉴6", param: "menu6" },
-      { id: 2, content: "메뉴7", param: "menu7" },
-      { id: 3, content: "메뉴8", param: "menu8" },
-    ],
-  },
-];
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Topics: NextPage = () => {
+  const router = useRouter();
+  const [state, setState] = useState({
+    lnbDatas: [
+      {
+        id: 0,
+        category: "카테고리1",
+        menus: [{ id: 0, content: "메뉴1", param: "menu1" }],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    getCategory();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router]);
+  const getCategory = async () => {
+    let categoryList: any = [];
+    await axios.get("/api2/category").then(res => {
+      const category = res.data.result;
+      category.map((item: object, idx: number) => {
+        categoryList.push({
+          id: category[idx].idx,
+          content: category[idx].bo_subject,
+          param: category[idx].bo_table,
+        });
+      });
+      setState({
+        ...state,
+        lnbDatas: [{ id: 1, category: "전체", menus: categoryList }],
+      });
+    });
+  };
   return (
     <>
       <LnbLayout>
-        <Lnb lnbDatas={lnbDatas} param="menu4" />
+        <Lnb lnbDatas={state.lnbDatas} param="menu4" />
         <Board />
       </LnbLayout>
     </>
