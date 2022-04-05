7a0dfebf0e724acaa469df24709b630104bc914d
limsoomin <lsmism10@icloud.com>
2022-04-05 13:54:58 +0900

    서치 더미 추가

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,11 +4,32 @@ import SnbLayout from "@components/layouts/snb-layout";
 import Snb from "@components/templates/snb";
 import Board from "@components/templates/board";
 
+const sampleSearchSnbDatas = [
+  {
+    id: 0,
+    category: "토픽 (100)",
+    menus: [
+      { id: 0, content: "메뉴 1 (20)", param: "menu1" },
+      { id: 1, content: "메뉴 2 (50)", param: "menu2" },
+      { id: 2, content: "메뉴 3 (30)", param: "menu3" },
+    ],
+  },
+  {
+    id: 0,
+    category: "인사이트 (10)",
+    menus: [
+      { id: 0, content: "메뉴 4 (5)", param: "menu4" },
+      { id: 1, content: "메뉴 5 (2)", param: "menu5" },
+      { id: 2, content: "메뉴 6 (3)", param: "menu6" },
+    ],
+  },
+];
+
 const Search: NextPage = () => {
   const router = useRouter();
   return (
     <SnbLayout>
-      <Snb />
+      <Snb category="menu1" snbDatas={sampleSearchSnbDatas} />
       {/* <Board
         category="test"
         // Datas={category == "인기글" ? hotTopic : topicListFilter}
