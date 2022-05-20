cbec9fb7b499f1667ae9dda20307332935f7547f
limsoomin <lsmism10@icloud.com>
2022-05-20 13:26:38 +0900

    서체 오류 수정

--- components/elements/types.tsx
+++ components/elements/types.tsx
@@ -11,7 +11,7 @@ export const Header1 = styled.h1<IPropsTypes>`
   line-height: ${props => props.theme.lineHeight.header1};
   font-weight: 700;
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -22,7 +22,7 @@ export const Header2 = styled.h2<IPropsTypes>`
   line-height: ${props => props.theme.lineHeight.header2};
   font-weight: 700;
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -33,7 +33,7 @@ export const Header3 = styled.h3<IPropsTypes>`
   line-height: ${props => props.theme.lineHeight.header3};
   font-weight: 700;
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -44,7 +44,7 @@ export const Header4 = styled.h4<IPropsTypes>`
   line-height: ${props => props.theme.lineHeight.header4};
   font-weight: 700;
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -55,7 +55,7 @@ export const Header5 = styled.h5<IPropsTypes>`
   line-height: ${props => props.theme.lineHeight.header5};
   font-weight: 700;
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -67,7 +67,7 @@ export const Sub1 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -79,7 +79,7 @@ export const Sub2 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -91,7 +91,7 @@ export const Sub3 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -103,7 +103,7 @@ export const Body1 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -115,7 +115,7 @@ export const Body2 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
@@ -127,7 +127,7 @@ export const Body3 = styled.p<IPropsTypes>`
   font-weight: ${props =>
     props.isBold ? "700" : props.isLink ? "500" : "400"};
   color: ${props => (props.color ? props.color : "inherit")};
-  cursor: ${props => (props.isLink ? "pointer" : "text")};
+  cursor: ${props => (props.isLink ? "pointer" : "inherit")};
   &:hover,
   &:focus {
     text-decoration: ${props => (props.isLink ? "underline" : "none")};
