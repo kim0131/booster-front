c25808b8dc0e0d6cfb2c35fc5748da759ca83286
limsoomin <lsmism10@icloud.com>
2022-05-26 15:01:13 +0900

    토스트 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,15 +8,14 @@ import global from "@components/styles/global";
 import useToast from "@core/hook/use-toast";
 import Header from "@components/templates/header";
 import Footer from "@components/templates/footer";
-
 import * as gtag from "../lib/gtag";
 import { useRouter } from "next/router";
 const isProduction = process.env.NODE_ENV === "production";
 import { useEffect } from "react";
 import { SWRConfig } from "swr";
+import Toast from "@components/elements/toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { message } = useToast();
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: URL) => {
@@ -120,9 +119,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Header />
           <Component {...pageProps} />
           <Footer />
-          <div>{message}</div>
           <div id="modal" />
-          <div id="toast" />
+          <Toast />
         </SWRConfig>
       </SessionProvider>
     </ThemeProvider>
